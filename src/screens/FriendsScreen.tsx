ab4fd3400e915cd18fff9fd80c981feab5c5ddb6
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-12-23 02:40:09 -0300

    Create FriendsScreen component

---
+++ src/screens/FriendsScreen.tsx
@@ -0,0 +1,48 @@
+import { Screen, ScreenHeader, ScreenMain } from "../components/Screen/Screen";
+import { ListItem, Avatar, Text, Icon } from "@rneui/themed";
+import { FlexibleScrollBox } from "../components/FlexibleBoxes/FlexibleBox";
+import appDataStructure from "../appDataStructure.json";
+import { StyleSheet } from "react-native";
+
+export default function FriendsScreen() {
+  const { friends } = appDataStructure;
+  return (
+    <Screen>
+      <ScreenHeader style={{ alignItems: "center" }}>
+        <Text h3>Friends</Text>
+      </ScreenHeader>
+      <ScreenMain>
+        <FlexibleScrollBox>
+          {friends.map((item, i) => (
+            <ListItem
+              containerStyle={styles.accountsContainer}
+              key={i}
+              bottomDivider
+            >
+              <Avatar
+                size={"large"}
+                rounded
+                icon={{ name: "person-circle-outline", type: "ionicon" }}
+                containerStyle={{ backgroundColor: "rgb(32, 137, 220)" }}
+              />
+              <ListItem.Content>
+                <ListItem.Title style={{ width: "100%" }}>
+                  <Text>{item.name.toUpperCase()}</Text>
+                </ListItem.Title>
+                <ListItem.Subtitle>
+                  <Text>{item.email.toUpperCase()}</Text>
+                </ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </FlexibleScrollBox>
+      </ScreenMain>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  accountsContainer: {
+    width: "90%",
+  },
+});
